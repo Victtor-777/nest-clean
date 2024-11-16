86cee9d88e083ba7ab300584380cf78e71cb19bc
Victtor777 <victtor.eng.software@gmail.com>
2024-11-16 16:25:20 -0300

    Criando o EnvModule

--- dist/src/infra/auth/auth.module.js
+++ dist/src/infra/auth/auth.module.js
@@ -8,12 +8,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AuthModule = void 0;
 const common_1 = require("@nestjs/common");
-const config_1 = require("@nestjs/config");
 const jwt_1 = require("@nestjs/jwt");
 const passport_1 = require("@nestjs/passport");
 const jwt_strategy_1 = require("./jwt.strategy");
 const core_1 = require("@nestjs/core");
 const jwt_auth_guard_1 = require("./jwt-auth.guard");
+const env_module_1 = require("../env/env.module");
+const env_service_1 = require("../env/env.service");
 let AuthModule = class AuthModule {
 };
 exports.AuthModule = AuthModule;
@@ -22,11 +23,12 @@ exports.AuthModule = AuthModule = __decorate([
         imports: [
             passport_1.PassportModule,
             jwt_1.JwtModule.registerAsync({
-                inject: [config_1.ConfigService],
+                imports: [env_module_1.EnvModule],
+                inject: [env_service_1.EnvService],
                 global: true,
-                useFactory(config) {
-                    const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true });
-                    const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
+                useFactory(env) {
+                    const privateKey = env.get('JWT_PRIVATE_KEY');
+                    const publicKey = env.get('JWT_PUBLIC_KEY');
                     return {
                         signOptions: { algorithm: 'RS256' },
                         privateKey: Buffer.from(privateKey, 'base64'),
@@ -37,6 +39,7 @@ exports.AuthModule = AuthModule = __decorate([
         ],
         providers: [
             jwt_strategy_1.JwtStrategy,
+            env_service_1.EnvService,
             {
                 provide: core_1.APP_GUARD,
                 useClass: jwt_auth_guard_1.JwtAuthGuard,
