3f6d8589f51acdaedd612692fffddbc486e08fbe
Victtor777 <victtor.eng.software@gmail.com>
2024-11-17 09:40:04 -0300

    Implementando repositórios

--- dist/src/infra/database/prisma/repositories/prisma-answer-attachments-repository.js
+++ dist/src/infra/database/prisma/repositories/prisma-answer-attachments-repository.js
@@ -5,19 +5,37 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PrismaAnswerAttachmentRepository = void 0;
 const common_1 = require("@nestjs/common");
+const prisma_service_1 = require("../prisma.service");
+const prisma_answer_attachment_mapper_1 = require("../mappers/prisma-answer-attachment-mapper");
 let PrismaAnswerAttachmentRepository = class PrismaAnswerAttachmentRepository {
-    findManyByAnswerId(answerId) {
-        throw new Error('Method not implemented.');
+    constructor(prisma) {
+        this.prisma = prisma;
     }
-    deleteManyByAnswerId(answerId) {
-        throw new Error('Method not implemented.');
+    async findManyByAnswerId(answerId) {
+        const answerAttachments = await this.prisma.attachment.findMany({
+            where: {
+                answerId,
+            },
+        });
+        return answerAttachments.map(prisma_answer_attachment_mapper_1.PrismaAnswerAttachmentMapper.toDomain);
+    }
+    async deleteManyByAnswerId(answerId) {
+        await this.prisma.attachment.deleteMany({
+            where: {
+                answerId,
+            },
+        });
     }
 };
 exports.PrismaAnswerAttachmentRepository = PrismaAnswerAttachmentRepository;
 exports.PrismaAnswerAttachmentRepository = PrismaAnswerAttachmentRepository = __decorate([
-    (0, common_1.Injectable)()
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [prisma_service_1.PrismaService])
 ], PrismaAnswerAttachmentRepository);
 //# sourceMappingURL=prisma-answer-attachments-repository.js.map
